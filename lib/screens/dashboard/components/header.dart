eda17e708a627e33765e6979842bfcb8a8177578
birdgang <birdgang82@gmail.com>
2022-07-13 18:09:05 +0900

    init admin

---
+++ lib/screens/dashboard/components/header.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+
+import '../../../constants.dart';
+import '../../../controllers/MenuController.dart';
+import '../../../responsive.dart';
+
+class Header extends StatelessWidget {
+  const Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (!Responsive.isDesktop(context))
+          IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: context.read<MenuController>().controlMenu,
+          ),
+        if (!Responsive.isMobile(context))
+          Text(
+            "Dashboard",
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        if (!Responsive.isMobile(context))
+          Spacer(flex: Responsive.isDesktop(context) ? 2 : 1),
+        const Expanded(child: SearchField()),
+      ],
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Search",
+        fillColor: secondaryColor,
+        filled: true,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        suffixIcon: InkWell(
+          onTap: () {},
+          child: Container(
+            padding: const EdgeInsets.all(defaultPadding * 0.75),
+            margin: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+            decoration: const BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: SvgPicture.asset("assets/icons/Search.svg"),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+import '../../responsive.dart';
+import 'components/header.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        primary: false,
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          children: [
+            const Header(),
+            const SizedBox(height: defaultPadding),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: defaultPadding),
+                      if (Responsive.isMobile(context))
+                        const SizedBox(height: defaultPadding),
+                    ],
+                  ),
+                ),
+                if (!Responsive.isMobile(context))
+                  const SizedBox(width: defaultPadding),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
